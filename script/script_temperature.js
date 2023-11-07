d9bb91039a0bbcd07616534db31aff24d8e782d2
AlexSansI <128729558+AlexSansI@users.noreply.github.com>
2023-11-07 14:54:12 +0100

    Add files via upload

---
+++ script/script_temperature.js
@@ -0,0 +1,28 @@
+var gauge2 = Gauge(
+    document.getElementById("gauge_temp"),
+    {
+      min: -50,
+      max: 50,
+      dialStartAngle: 180,
+      dialEndAngle: 0,
+      value: 0,
+      viewBox: "0 0 100 57",
+      color: function(value) {
+        if (value >=20){
+          return "#d21404";
+        }
+        else if (value >=10){
+          return "#03c04a";
+        }
+        else if (value >=0){
+          return "#82eefd";
+        }
+        else if (value >=-20){
+          return "#0492c2";
+        }
+        else {
+          return "#0000ff";
+        }
+      }
+    }
+    );
